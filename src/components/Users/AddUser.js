d6570f3b04eb236dbb1ffb2e5bd23ed5693afbf8
Prasad Surase <prasad.surase@coupa.com>
2022-10-03 12:45:19 +0530

    Added validation for user name and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(1);
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    if(username.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    setUsername('')
+    setAge(1);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
   };
 
   return(
     <Card className={classes.input}>
       <form onSubmit={onSubmitHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" value={username} onChange={usernameChangeHandler}/>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" value={age} onChange={ageChangeHandler}/>
         <Button buttonType="submit">Add User</Button>
       </form>
     </Card>
